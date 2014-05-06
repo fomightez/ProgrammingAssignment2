4919cdb7d97105aca5e210b98c0792341be943d2
Wayne's Bioinformatics Code Portal is <fomightez@gmail.com>
2014-05-05 23:51:11 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,17 @@
 ## makeCacheMatrix creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
 }
 
 
@@ -11,4 +21,13 @@ makeCacheMatrix <- function(x = matrix()) {
 ## retrieve the inverse from the cache.
 cacheSolve <- function(x, ...) {
         ## ReturnS a matrix that is the inverse of 'x'
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setmean(m)
+        m
 }
